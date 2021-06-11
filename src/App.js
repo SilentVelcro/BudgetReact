bd019e30e45bbf0ffe457dd1f26d9d4d5d0720f3
Nikki Grostefon <73565265+NikkiGrostefon@users.noreply.github.com>
2021-06-11 19:44:33 -0400

    section 51 completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
@@ -33,7 +34,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
